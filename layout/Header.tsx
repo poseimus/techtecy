d6115eb47b226b5b96260860d4f2aeca1ea9791d
Emir Akyüz <emirakyuz81@gmail.com>
2024-01-18 21:16:46 +0300

    # Update
    -...test

--- layout/Header.tsx
+++ layout/Header.tsx
@@ -3,7 +3,7 @@ export default function Header() {
         <section className="text-black font-semibold text-lg bg-white dark:text-white dark:bg-black sm:border-1">
             <div className="container mx-auto flex flex-row gap-3 py-3 items-center sm:justify-between">
                 <div className="">
-                    asdfasd
+                    Test
                 </div> 
                 <div className="">
                     asdfasd
